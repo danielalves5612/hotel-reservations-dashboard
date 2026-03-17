ca7ef848fb68510fd2d2402d53554ede0fdc4396
Daniel Alves de Souza <161609355+danielalves5612@users.noreply.github.com>
2026-03-17 19:01:41 -0300

    Refinamentos no código

--- assets/modules/reservas.js
+++ assets/modules/reservas.js
@@ -1,42 +1,18 @@
 import ValidaCPF from './validaCPF.js'
 let reservas = []
 
-/*
-   {
-        id: 1,
-        nome: 'João Silva',
-        quarto: 'Quarto Duplo',
-        checkIn: '2026-03-11',
-        checkOut: '2026-03-13',
-        status: 'confirmada'
-    },
-    {
-        id: 2,
-        nome: 'Daniel Alves',
-        quarto: 'Quarto Triplo',
-        checkIn: '2026-03-25',
-        checkOut: '2026-03-26',
-        status: 'cancelada'
-    },
-    {
-        id: 3,
-        nome: 'Gabriel Souza',
-        quarto: 'Quarto Single',
-        checkIn: '2026-03-01',
-        checkOut: '2026-03-06',
-        status: 'pendente'
-    }*/
-
 const inputBusca = document.querySelector('#buscar-reserva')
+
+// Estado
 let reservaEmEdicao = null
 
+// Evento do InputBusca
 inputBusca.addEventListener('input', function(e){
     const termoBusca = inputBusca.value.toLowerCase().trim()
     filtrarReservas(termoBusca)
-
 })
 
-
+// Evento Global
 document.addEventListener('click', function(e){
     const el = e.target
     if(el.classList.contains('btn-nova-reserva')){
@@ -77,19 +53,25 @@ document.addEventListener('click', function(e){
     }
 })
 
+// Estorage
 function salvarReservas(){
     const json = JSON.stringify(reservas)
     localStorage.setItem('reservas', json)
 }
 
+// Estorage
 function exibirReservas(){
     const reservasSalvas = localStorage.getItem('reservas')
-    const json = JSON.parse(reservasSalvas)
-    reservas = json
-    salvarReservas()
+    if(reservasSalvas === null){
+        reservas = []
+    }else{
+        const json = JSON.parse(reservasSalvas)
+        reservas = json
+    }
     renderizarReservas(reservas)
 }
 
+// CRUD
 function filtrarReservas(termoBusca){
     const reservasFiltradas = reservas.filter(reserva => 
         reserva.nome.toLowerCase().includes(termoBusca) ||
@@ -104,15 +86,65 @@ function filtrarReservas(termoBusca){
     renderizarReservas(reservasFiltradas)
 }
 
+// Renderizar
 function capitalizar(texto){
     return texto[0].toUpperCase() + texto.slice(1)
 }
 
+function exibirData(data){
+    const [ano, mes, dia] = data.split('-')
+    return `${dia}/${mes}/${ano}`
+}
+
+// Modal
 function fecharModal(){
     const overlay = document.querySelector('.overlay')
     overlay.remove()
 }
 
+function criaCampoInput(form, labelFor, labelText, classInput, inputType, inputId, inputPlaceHolder){
+    const div = document.createElement('div')
+    div.classList.add('div-campo')
+    const label = document.createElement('label')
+    label.htmlFor = labelFor
+    label.textContent = labelText
+    div.appendChild(label)
+    const input = document.createElement('input')
+    input.classList.add(`${classInput}`)
+    input.type = inputType
+    input.id = inputId
+    input.placeholder = inputPlaceHolder
+    div.appendChild(input)
+    form.appendChild(div)
+}
+
+function criaCampoSelect(form, labelFor, labelText, classSelect, selectId, optionTextUm, optionTextDois, optionTextTres, optionValueUm, optionValueDois, optionValueTres){
+    const div = document.createElement('div')
+    div.classList.add('div-campo')
+    const label = document.createElement('label')
+    label.htmlFor = labelFor
+    label.textContent = labelText
+    const select = document.createElement('select')
+    select.classList.add(`${classSelect}`)
+    select.id = selectId
+    const optionUm = document.createElement('option')
+    optionUm.textContent = optionTextUm
+    optionUm.value = optionValueUm
+    const optionDois = document.createElement('option')
+    optionDois.textContent = optionTextDois
+    optionDois.value = optionValueDois
+    const optionTres = document.createElement('option')
+    optionTres.textContent = optionTextTres
+    optionTres.value = optionValueTres
+    select.appendChild(optionUm)
+    select.appendChild(optionDois)
+    select.appendChild(optionTres)
+    div.appendChild(label)
+    div.appendChild(select)
+    form.appendChild(div)
+}
+
+// Modal
 function criarModal(){
     const overlay = document.createElement('div')
     overlay.classList.add('overlay')
@@ -120,116 +152,26 @@ function criarModal(){
     const modal = document.createElement('div')
     modal.classList.add('modal')
     overlay.appendChild(modal)
+
     const h1 = document.createElement('h1')
     h1.textContent = 'Cadastro de Reserva'
     modal.appendChild(h1)
+
     const form = document.createElement('form')
     form.classList.add('form-reserva')
     modal.appendChild(form)
 
-    const divNome = document.createElement('div')
-    divNome.classList.add('div-campo')
-    form.appendChild(divNome)
-    const labelNome = document.createElement('label')
-    labelNome.htmlFor = 'nome-hospede'
-    labelNome.textContent = 'Nome'
-    divNome.appendChild(labelNome)
-    const inputNome = document.createElement('input')
-    inputNome.classList.add('input-nome')
-    inputNome.type = 'text'
-    inputNome.id = 'nome-hospede'
-    divNome.appendChild(inputNome)
-    inputNome.placeholder = 'Nome do hóspede'
-    const divCpf = document.createElement('div')
-    divCpf.classList.add('div-campo')
-    form.appendChild(divCpf)
-    const labelCpf = document.createElement('label')
-    labelCpf.htmlFor = 'cpf-hospede'
-    labelCpf.textContent = 'CPF'
-    divCpf.appendChild(labelCpf)
-    const inputCpf = document.createElement('input')
-    inputCpf.classList.add('input-cpf')
-    inputCpf.type = 'number'
-    inputCpf.id = 'cpf-hospede'
-    divCpf.appendChild(inputCpf)
-    const divCheckIn = document.createElement('div')
-    divCheckIn.classList.add('div-campo')
-    form.appendChild(divCheckIn)
-    const labelCheckIn = document.createElement('label')
-    labelCheckIn.htmlFor = 'data-checkin'
-    labelCheckIn.textContent = 'Check-In'
-    divCheckIn.appendChild(labelCheckIn)
-    const inputCheckIn = document.createElement('input')
-    inputCheckIn.classList.add('input-checkIn')
-    inputCheckIn.type = 'date'
-    inputCheckIn.id = 'data-checkin'
-    divCheckIn.appendChild(inputCheckIn)
-    const divCheckOut = document.createElement('div')
-    divCheckOut.classList.add('div-campo')
-    form.appendChild(divCheckOut)
-    const labelCheckOut = document.createElement('label')
-    labelCheckOut.htmlFor = 'data-checkout'
-    labelCheckOut.textContent = 'Check-Out'
-    divCheckOut.appendChild(labelCheckOut)
-    const inputCheckOut = document.createElement('input')
-    inputCheckOut.classList.add('input-checkOut')
-    inputCheckOut.type = 'date'
-    inputCheckOut.id = 'data-checkout'
-    divCheckOut.appendChild(inputCheckOut)
+    criaCampoInput(form, 'nome-hospede', 'Nome', 'input-nome', 'text', 'nome-hospede', 'Nome do hóspede')
+    criaCampoInput(form, 'cpf-hospede', 'CPF', 'input-cpf', 'number', 'cpf-hospede', 'Digite o CPF')
+    criaCampoInput(form, 'data-checkin', 'Check-in', 'input-checkIn', 'date', 'data-checkin')
+    criaCampoInput(form, 'data-checkout', 'Check-Out', 'input-checkOut', 'date', 'data-checkout')
 
-    const divQuarto = document.createElement('div')
-    divQuarto.classList.add('.div-campo')
-    form.appendChild(divQuarto)
-    const labelQuarto = document.createElement('label')
-    labelQuarto.htmlFor = 'quarto-hospede'
-    labelQuarto.textContent = 'Quarto'
-    divQuarto.appendChild(labelQuarto)
-    const selectQuarto = document.createElement('select')
-    selectQuarto.classList.add('select-quarto')
-    selectQuarto.id = 'quarto-hospede'
-    divQuarto.appendChild(selectQuarto)
-    const optionSingle = document.createElement('option')
-    optionSingle.textContent = 'Quarto Single'
-    optionSingle.value = 'single'
-    const optionDuplo = document.createElement('option')
-    optionDuplo.textContent = 'Quarto Duplo'
-    optionDuplo.value = 'duplo'
-    const optionTriplo = document.createElement('option')
-    optionTriplo.textContent = 'Quarto Triplo'
-    optionTriplo.value = 'triplo'
-
-    selectQuarto.appendChild(optionSingle)
-    selectQuarto.appendChild(optionDuplo)
-    selectQuarto.appendChild(optionTriplo)
-
-    const divStatus = document.createElement('div')
-    divStatus.classList.add('div-campo')
-    form.appendChild(divStatus)
-    const labelStatus = document.createElement('label')
-    labelStatus.htmlFor = 'status-reserva'
-    labelStatus.textContent = 'Status'
-    divStatus.appendChild(labelStatus)
-    const selectStatus = document.createElement('select')
-    selectStatus.id = 'status-reserva'
-    selectStatus.classList.add('select-status')
-    divStatus.appendChild(selectStatus)
-    const optionConfirmada = document.createElement('option')
-    optionConfirmada.textContent = 'Confirmada'
-    optionConfirmada.value = 'confirmada'
-    const optionCancelada = document.createElement('option')
-    optionCancelada.textContent = 'Cancelada'
-    optionCancelada.value = 'cancelada'
-    const optionPendente = document.createElement('option')
-    optionPendente.textContent = 'Pendente'
-    optionPendente.value = 'pendente'
-
-    selectStatus.appendChild(optionPendente)
-    selectStatus.appendChild(optionCancelada)
-    selectStatus.appendChild(optionConfirmada)
-
-    const divBotoes = document.createElement('div')
-    divBotoes.classList.add('div-campo')
-    form.appendChild(divBotoes)
+    criaCampoSelect(form, 'quarto-hospede', 'Quarto', 'select-quarto', 'quarto-hospede', 'Quarto Single', 'Quarto Duplo', 'Quarto Triplo', 'single', 'duplo', 'triplo')
+    criaCampoSelect(form, 'status-reserva', 'Status', 'select-status', 'status-reserva', 'Confirmada', 'Cancelada', 'Pendente', 'confirmada', 'cancelada', 'pendente')
+ 
+    const div = document.createElement('div')
+    div.classList.add('div-campo')
+    form.appendChild(div)
     const botaoSalvar = document.createElement('button')
     botaoSalvar.classList.add('button-salvar')
     botaoSalvar.type = 'submit'
@@ -238,8 +180,8 @@ function criarModal(){
     botaoCancelar.classList.add('button-cancelar')
     botaoCancelar.type = 'button'
     botaoCancelar.textContent = 'Cancelar'
-    divBotoes.appendChild(botaoSalvar)
-    divBotoes.appendChild(botaoCancelar)
+    div.appendChild(botaoSalvar)
+    div.appendChild(botaoCancelar)
 
     const body = document.body
 
@@ -247,49 +189,73 @@ function criarModal(){
 
     form.addEventListener('submit', function(e){
         e.preventDefault()
-        let temErro = false
+        handleSubmit()
+        
+})
+
+function validarFormulario(nome, cpf, checkIn, checkOut){
+    let temErro = false
+    const cpfValido =  new ValidaCPF(cpf)
+    const cpfInputado = cpfValido.valida()
+    if(!nome) {
+        mensagemErro(inputNome, 'O Nome deve ser preenchido.')
+
+        temErro = true
+    }
+
+    if(!cpf){
+        mensagemErro(inputCpf, 'O CPF precisa ser preenchido.')
+
+        temErro = true
+
+    }else if(cpfInputado === false){
+        mensagemErro(inputCpf, 'Por favor, digite um CPF válido.')
+
+        temErro = true
+    }
+
+    if(!checkIn){
+        mensagemErro(inputCheckIn, 'Selecione uma data para o Check-In.')
+
+        temErro = true
+    }
+
+    if(!checkOut){
+        mensagemErro(inputCheckOut, 'Selecione uma data para o Check-Out.')
+
+        temErro = true
+    }
+        
+    return temErro === true
+}
+
+function handleSubmit(){
         const nome = inputNome.value
         const cpf = inputCpf.value
         const checkIn = inputCheckIn.value
         const checkOut = inputCheckOut.value
+        const selectQuarto = form.querySelector('.select-quarto')
+        const selectStatus = form.querySelector('.select-status')
         const quarto = selectQuarto.value
         const status = selectStatus.value
-        const cpfValido =  new ValidaCPF(cpf)
-        const cpfInputado = cpfValido.valida()
-        if(!nome) {
-            mensagemErro(inputNome, 'O Nome deve ser preenchido.')
-            temErro = true
-        }
-        if(!cpf){
-            mensagemErro(inputCpf, 'O CPF precisa ser preenchido.')
-            temErro = true
-        }else if(cpfInputado === false){
-            mensagemErro(inputCpf, 'Por favor, digite um CPF válido.')
-            temErro = true
-        }
-        if(!checkIn){
-            mensagemErro(inputCheckIn, 'Selecione uma data para o Check-In.')
-            temErro = true
-        }
-        if(!checkOut){
-            mensagemErro(inputCheckOut, 'Selecione uma data para o Check-Out.')
-            temErro = true
-        }
-        if(temErro === true){
-            return
-        }
-        const cpfExiste = reservas.some(reserva => reserva.cpf === cpf)
-        const cpfEditado = reservas.some(reserva => reserva.id !== reservaEmEdicao.id)
+        const temErro = validarFormulario(nome, cpf, checkIn, checkOut)
+
+        if(temErro) return 
+        
         if(reservaEmEdicao === null){
-            if(cpfExiste === true){
+            const cpfExiste = reservas.some(reserva => reserva.cpf === cpf)
+            if(cpfExiste){
                 mensagemErro(inputCpf, 'CPF cadastrado em outra Reserva')
                 return
             }
-        }else if(reservaEmEdicao !== true){
-            if(cpfEditado === true){
+            }else{
+                const cpfExiste = reservas.some(reserva => reserva.cpf === cpf && reserva.id !== reservaEmEdicao.id)
+                if(cpfExiste){
                 mensagemErro(inputCpf, 'CPF cadastrado em outra Reserva')
+                return
+                }
             }
-        }
+
         if(reservaEmEdicao === null){
             const novaReserva = {
             id: reservas.length + 1,
@@ -316,7 +282,12 @@ function criarModal(){
             fecharModal()
             reservaEmEdicao = null
         }
-    })
+    }
+
+    const inputNome = form.querySelector('.input-nome')
+    const inputCpf = form.querySelector('.input-cpf')
+    const inputCheckIn = form.querySelector('.input-checkIn')
+    const inputCheckOut = form.querySelector('.input-checkOut')
 
     inputNome.addEventListener('input', function(e){
         const el = e.target
@@ -336,6 +307,7 @@ function criarModal(){
     })
 }
 
+// Validação
 function mensagemErro(campo, mensagem){
     const div = campo.closest('.div-campo')
     const pErro = div.querySelector('.error-message')
@@ -350,6 +322,7 @@ function mensagemErro(campo, mensagem){
     }
 }
 
+// Validação
 function limparErro(campo){
     const div = campo.closest('.div-campo')
     const p = div.querySelector('.error-message')
@@ -358,6 +331,7 @@ function limparErro(campo){
     }
 }
 
+// Renderização
 function atualizarResumo(){
     const numeroHoje = document.querySelector('.numero-hoje')
     const numeroPendentes = document.querySelector('.numero-pendentes')
@@ -374,49 +348,59 @@ function atualizarResumo(){
     numeroHoje.textContent = reservasHoje.length
 }
 
+// Renderização
+
+function criaCardReserva(reserva, reservasContainer){
+    const cardReserva = document.createElement('div')
+    cardReserva.classList.add('card-reserva')
+    reservasContainer.appendChild(cardReserva)
+    const fotoHospede = document.createElement('div')
+    fotoHospede.classList.add('foto-hospede')
+    cardReserva.appendChild(fotoHospede)
+    const imgPerfil = document.createElement('img')
+    fotoHospede.appendChild(imgPerfil)
+    imgPerfil.src = 'assets/img/foto-perfil.jpeg'
+    imgPerfil.alt = `Foto de ${reserva.nome}`
+    const infoReserva = document.createElement('div')
+    infoReserva.classList.add('info-reserva')
+    cardReserva.appendChild(infoReserva)
+    const numeroReserva = document.createElement('small')
+    cardReserva.dataset.id = reserva.id
+    numeroReserva.classList.add('numero-reserva')
+    numeroReserva.textContent = `Reserva #00${reserva.id}` 
+    infoReserva.appendChild(numeroReserva)
+    const nomeHospede = document.createElement('h3')
+    infoReserva.appendChild(nomeHospede)
+    nomeHospede.textContent = reserva.nome
+    const infoQuarto = document.createElement('p')
+    infoReserva.appendChild(infoQuarto)
+    infoQuarto.textContent = `Quarto ${capitalizar(reserva.quarto)}`
+    const infoData = document.createElement('p')
+    infoData.textContent = `${exibirData(reserva.checkIn)} ° ${exibirData(reserva.checkOut)}`
+    infoReserva.appendChild(infoData)
+    const status = document.createElement('span')
+    status.classList.add('status', reserva.status)
+    status.textContent = capitalizar(reserva.status)
+    cardReserva.appendChild(status)
+    const acoes = document.createElement('div')
+    acoes.classList.add('acoes-reserva')
+    cardReserva.appendChild(acoes)
+    const botaoExcluir = document.createElement('button')
+    botaoExcluir.classList.add('button-excluir')
+    botaoExcluir.textContent = 'Excluir'
+    acoes.appendChild(botaoExcluir)
+    const botaoEditar = document.createElement('button')
+    botaoEditar.classList.add('button-editar')
+    botaoEditar.textContent = 'Editar'
+    acoes.appendChild(botaoEditar)
+}
+
+
 export default function renderizarReservas(listaReservas = reservas){
     const reservasContainer = document.querySelector('.reservas-container')
     reservasContainer.innerHTML = ''
     for(let reserva of listaReservas){
-        const cardReserva = document.createElement('div')
-        cardReserva.classList.add('card-reserva')
-        reservasContainer.appendChild(cardReserva)
-        const fotoHospede = document.createElement('div')
-        fotoHospede.classList.add('foto-hospede')
-        cardReserva.appendChild(fotoHospede)
-        const imgPerfil = document.createElement('img')
-        fotoHospede.appendChild(imgPerfil)
-        imgPerfil.src = 'assets/img/foto-perfil.jpeg'
-        imgPerfil.alt = `Foto de ${reserva.nome}`
-        const infoReserva = document.createElement('div')
-        infoReserva.classList.add('info-reserva')
-        cardReserva.appendChild(infoReserva)
-        const numeroReserva = document.createElement('small')
-        cardReserva.dataset.id = reserva.id
-        numeroReserva.classList.add('numero-reserva')
-        numeroReserva.textContent = `Reserva #00${reserva.id}` 
-        infoReserva.appendChild(numeroReserva)
-        const nomeHospede = document.createElement('h3')
-        infoReserva.appendChild(nomeHospede)
-        nomeHospede.textContent = reserva.nome
-        const infoQuarto = document.createElement('p')
-        infoReserva.appendChild(infoQuarto)
-        infoQuarto.textContent = `Quarto ${capitalizar(reserva.quarto)} ${reserva.checkIn} - ${reserva.checkOut}`
-        const status = document.createElement('span')
-        status.classList.add('status', reserva.status)
-        status.textContent = capitalizar(reserva.status)
-        cardReserva.appendChild(status)
-        const acoes = document.createElement('div')
-        acoes.classList.add('acoes-reserva')
-        cardReserva.appendChild(acoes)
-        const botaoExcluir = document.createElement('button')
-        botaoExcluir.classList.add('button-excluir')
-        botaoExcluir.textContent = 'Excluir'
-        acoes.appendChild(botaoExcluir)
-        const botaoEditar = document.createElement('button')
-        botaoEditar.classList.add('button-editar')
-        botaoEditar.textContent = 'Editar'
-        acoes.appendChild(botaoEditar)
+        criaCardReserva(reserva, reservasContainer)
     }
     atualizarResumo()
     salvarReservas()
